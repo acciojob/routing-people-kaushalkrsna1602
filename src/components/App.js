4696eeff83e24013db31555bb04daa38932c7c4f
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-01-22 20:15:37 +0530

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,38 @@
-
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
-import {BrowserRouter as Router,Routes,Route,NavLink} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserDetails from "./UserDetails";
 import Users from "./Users";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [users,setUsers] = useState([]);
   // Fetch data when the component mounts for the first time
-
-  useEffect(()=>{
-
-     fetch("https://jsonplaceholder.typicode.com/users")
-     .then(resp=>resp.json())
-     .then(data=>setUsers(data))
-   
-
-
-  },[])
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        
-       
-      
-        <Router>
+      {/* Do not remove the main div */}
+      <Router>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
           <Routes>
-            {
-              users.map((user)=>(
-                <>
-
-                
-                <Route path="/" element={<UserDetails users ={users} />}/>
-                <Route path="/users/:id" element={<Users users = {users}/>}/>
-                </>
-              ))
-            }
+            <Route path="/" element={<UserDetails users={users} />} />
+            <Route path="/users/:id" element={<Users users={users} />} />
           </Routes>
-        </Router>
-      
+        )}
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
